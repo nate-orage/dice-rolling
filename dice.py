10279662fa710275ded377dceb1256029ae66e36
nate-orage <nate54o@icloud.com>
2021-10-12 22:39:40 +0000

    Added 7 out.

--- dice.py
+++ dice.py
@@ -1,22 +1,29 @@
 #!/home/nate/python/python_env/bin/python3
 import random
 
-name = input(f'What''s your name?\n')
-welcome = f'Hello, {name}!\n\nWelcome to Dice Rolling.\nYou will be rolling two six-sided dice.'
+name = input(f"What" "s your name?\n")
+welcome = f"Hello, {name}!\n\nWelcome to Dice Rolling.\nYou will be rolling two six-sided dice."
 die_6_1 = [1, 2, 3, 4, 5, 6]
 die_6_2 = [1, 2, 3, 4, 5, 6]
 
+
 def begin():
     while True:
-        quit = ['quit', 'Quit']
-        d = ''
+        quit = ["quit", "Quit"]
+        d = ""
         d = input(f'\nPress Enter to roll your dice. Type "quit" to quit.\n')
         if d in quit:
             break
+        random.shuffle(die_6_1)
+        random.shuffle(die_6_2)
+        if (die_6_1[-3] + die_6_2[-3]) == 7:
+            print(
+                f"\nYou rolled {die_6_1[-3]} and {die_6_2[-3]}.\n\nSeven out. Game over.\n"
+            )
+            break
         else:
-            random.shuffle(die_6_1)
-            random.shuffle(die_6_2)
-            print(f'You rolled\n\n {die_6_1[-3]} and {die_6_2[-3]}.\n')
+            print(f"You rolled\n\n {die_6_1[-3]} and {die_6_2[-3]}.\n")
+
 
 print(welcome)
 begin()
