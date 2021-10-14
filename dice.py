c5805470aec7e7435f4faa0f6f3094d3c91d0346
nate-orage <nate54o@icloud.com>
2021-10-14 01:52:09 +0000

    A bit of simplifying

--- dice.py
+++ dice.py
@@ -7,23 +7,27 @@ die_6_1 = [1, 2, 3, 4, 5, 6]
 die_6_2 = [1, 2, 3, 4, 5, 6]
 
 
-def begin():
+def six_sided_2_dice():
     while True:
         quit = ["quit", "Quit"]
-        d = ""
-        d = input(f'\nPress Enter to roll your dice. Type "quit" to quit.\n')
-        if d in quit:
-            break
+        x = ""
+        x = input(f'\nPress Enter to roll your dice. Type "quit" to quit.\n')
         random.shuffle(die_6_1)
         random.shuffle(die_6_2)
+        game_over = (
+            f"\nYou rolled {die_6_1[-3]} and {die_6_2[-3]}.\n\nSeven out. Game over.\n"
+        )
+        normal_outcome = f"\nYou rolled\n\n {die_6_1[-3]} and {die_6_2[-3]}.\n"
+        if x in quit:
+            break
+        print(die_6_1)
+        print(die_6_2)
         if (die_6_1[-3] + die_6_2[-3]) == 7:
-            print(
-                f"\nYou rolled {die_6_1[-3]} and {die_6_2[-3]}.\n\nSeven out. Game over.\n"
-            )
+            print(game_over)
             break
         else:
-            print(f"You rolled\n\n {die_6_1[-3]} and {die_6_2[-3]}.\n")
+            print(normal_outcome)
 
 
 print(welcome)
-begin()
+six_sided_2_dice()
